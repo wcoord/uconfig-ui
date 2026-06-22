0cae64a27f0fd215a120746a5547b1e779566afb
Devan Carpenter <devan@informatics.coop>
2026-06-22 05:45:07 -0500

    build: add nix derivation
    This adds a nix derivation which pins nodejs 26.
    
    Run "nix-build" from the root of this repo to build the UI.
    
    Signed-off-by: Devan Carpenter <devan@informatics.coop>

---
+++ default.nix
@@ -0,0 +1,38 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  inherit (pkgs)
+    lib
+    nodejs_26
+    buildNpmPackage
+    ;
+in
+
+buildNpmPackage (finalAttrs: {
+  name = "uconfig-builder";
+
+  src = lib.cleanSource ./.;
+
+  nodejs = nodejs_26;
+
+  postPatch = ''
+    cp ${./package-lock.json} package-lock.json
+  '';
+
+  npmDepsHash = "sha256-e+TdjJLgy5kXkrfpwTH2hQybHIS68lvUaPrYt9jsnrQ=";
+  npmPackFlags = [ "--ignore-scripts" ]; # Unclear if this is necessary
+
+  dontNpmInstall = true;
+  installPhase = ''
+    runHook preInstall
+
+    cp -r dist $out
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "uconfig configuration builder UI";
+  };
+})
