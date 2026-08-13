62cb1a00ebf6a780f4f8f6644f0177001d993231
Zach Mandeville <webmaster@coolguy.website>
2026-08-14 11:17:48 +1200

    build: add commit hash as small footer to app
    It uses our vite build system's ability to define global variables,
    plus node's childprocess library, to grab the git commit hash and set it
    as a variable we can use across the app.
    
    Co-authored-by: Devan Carpenter <devan@informatics.coop>
    Signed-off-by: Devan Carpenter <devan@informatics.coop>

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
+import { execSync } from 'child_process';
+
+const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
 
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
+  define: {
+    __COMMIT_HASH__: JSON.stringify(commitHash)
+  },
   base: './',
   server: { host: true },
   preview: { host: true }
